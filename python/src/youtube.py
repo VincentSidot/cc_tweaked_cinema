3fc5805f32487f3e1f2c632dbe433e061cc78af5
Vincent S <vs38190@gmail.com>
2023-08-15 23:50:21 +0200

    Last release, with fix of cypher.py from pytube

--- python/src/youtube.py
+++ python/src/youtube.py
@@ -4,12 +4,11 @@ import subprocess
 import random
 import argparse
 import os
-import numpy as np
+import websockets
 from websockets.server import serve
 from pytube import YouTube
 from PIL import Image
 from time import time
-from pydub import AudioSegment
 
 
 def parse_args():
@@ -94,7 +93,12 @@ def download_video(url, base_folder="."):
 
 
 def get_video_id_from_url(url):
-    return url.split("=")[-1].split("&")[0]
+    video_id = None
+    if "v=" in url:
+        video_id = url.split("v=")[1].split("&")[0]
+    elif "youtu.be/" in url:
+        video_id = url.split("youtu.be/")[1].split("?")[0]
+    return video_id
 
 
 class ImageEncoder:
@@ -145,6 +149,7 @@ class VideoEncoder:
         self.video_path = video_path
         self.last_frame_time = None
         self.frame_remainder = 0.0
+        self.last_frame = None
 
     def start(self):
         self.cap = cv2.VideoCapture(self.video_path)
@@ -171,6 +176,10 @@ class VideoEncoder:
     def get_next_frame(self, width, height, ignored_frames=None):
         if ignored_frames is None:
             ignored_frames = self.estimate_frame_to_skip()
+        if ignored_frames < 0 and self.last_frame is not None:
+            # we are rendering too fast
+            # so we need to send the same frame again
+            return self.last_frame, 0, 0
         begin_time = time()
         if self.cap is None:
             raise Exception("Encoder not started")
@@ -181,6 +190,7 @@ class VideoEncoder:
             return None, 0, 0
         image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         image_encoded = ImageEncoder(image).encode(width, height)
+        self.last_frame = image_encoded
         end_time = time()
         return image_encoded, (end_time - begin_time)*1000, ignored_frames
         # return the time it takes to generate the frame (in milliseconds)
@@ -190,141 +200,38 @@ class VideoEncoder:
 
 
 class AudioEncoder:
-    def __init__(self, audio_path):
+    def __init__(self, audio_path, mode):
         self.audio_path = audio_path
         download_folder = os.path.dirname(audio_path)
-        self.output_left_path = f"{download_folder}/output_left.dfpwm"
-        self.output_right_path = f"{download_folder}/output_right.dfpwm"
-        if os.path.isfile(self.output_left_path):
-            os.remove(self.output_left_path)
-        if os.path.isfile(self.output_right_path):
-            os.remove(self.output_right_path)
-        if os.path.isfile(self.output_left_path) and os.path.isfile(self.output_right_path):
-            print("Audio already encoded")
-        else:
-            begin_time = time()
-            subprocess.call([
-                "./bin/dfpwm_encoder",
-                self.audio_path,
-                self.output_left_path,
-                self.output_right_path,
-            ], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-            end_time = time()
-            print(f"Audio encoded in {end_time - begin_time} seconds")
+        if mode == "mono":
+            self.output_left_path = f"{download_folder}/output_left.dfpwm"
+            self.output_right_path = f"{download_folder}/output_right.dfpwm"
+            if os.path.isfile(self.output_left_path) and os.path.isfile(self.output_right_path):
+                print("Audio already encoded")
+            else:
+                begin_time = time()
+                subprocess.call([
+                    "./bin/dfpwm_encoder",
+                    self.audio_path,
+                    self.output_left_path,
+                    self.output_right_path,
+                ], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+                end_time = time()
+                print(f"Audio encoded in {end_time - begin_time} seconds")
+        elif mode == "stereo":
+            self.output_path = f"{download_folder}/output.dfpwm"
+            if os.path.isfile(self.output_path):
+                print("Audio already encoded")
+            else:
+                begin_time = time()
+                subprocess.call([
+                    "./bin/dfpwm_encoder",
+                    self.audio_path,
+                    self.output_path,
+                ], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+                end_time = time()
+                print(f"Audio encoded in {end_time - begin_time} seconds")
 
-    def frame_generator(self, sample_size, side):
-        if side == "left":
-            path = self.output_left_path
-        else:
-            path = self.output_right_path
-        with open(path, "rb") as f:
-            while True:
-                data = f.read(sample_size)
-                if len(data) == 0:
-                    break
-                yield data
-
-# class AudioEncoder:
-#     SAMPLE_RATE = 48000
-#     PREC = 10
-#
-#     def __init__(self, audio_path):
-#         self.audio_path = audio_path
-#
-#     async def encode_dfpwm(self, input_data, sample_size, queue):
-#         charge = 0
-#         strength = 0
-#         previous_bit = False
-#         out_length = len(input_data) // 8
-#
-#         out = np.zeros(sample_size, dtype=np.uint8)
-#
-#         PREC = self.PREC
-#         PREC_MINUS_8 = PREC - 8
-#         ONE_SHIFT_PREC_THEN_MINUS_1 = (1 << PREC) - 1
-#         ONE_SHIFT_PREC_MINUS_1 = 1 << (PREC - 1)
-#
-#         for i in range(out_length):
-#             this_byte = 0
-#
-#             for j in range(8):
-#                 level = int(input_data[i * 8 + j] * 127)
-#
-#                 current_bit = (level > charge) or (level == charge and charge == 127)
-#                 target = 127 if current_bit else -128
-#
-#                 next_charge = charge + ((strength * (target - charge) + ONE_SHIFT_PREC_MINUS_1) >> PREC)
-#                 if next_charge == charge and next_charge != target:
-#                     next_charge += 1 if current_bit else -1
-#
-#                 z = ONE_SHIFT_PREC_THEN_MINUS_1 if current_bit == previous_bit else 0
-#                 next_strength = strength
-#                 if strength != z:
-#                     next_strength += 1 if current_bit == previous_bit else -1
-#                 if next_strength < 2 << PREC_MINUS_8:
-#                     next_strength = 2 << PREC_MINUS_8
-#
-#                 charge = next_charge
-#                 strength = next_strength
-#                 previous_bit = current_bit
-#
-#                 this_byte = (this_byte >> 1) + 128 if current_bit else this_byte >> 1
-#             out[i % sample_size] = this_byte
-#             if i % sample_size == sample_size-1:
-#                 await queue.put(out)
-#
-#     @staticmethod
-#     def pydub_to_np(audio):
-#         """
-#         Converts pydub audio segment into np.float32 of shape [duration_in_seconds*sample_rate, channels],
-#         where each value is in range [-1.0, 1.0].
-#         Returns tuple audio_np_array.
-#         """
-#         channel_sounds = audio.split_to_mono()
-#         samples = [s.get_array_of_samples() for s in channel_sounds]
-#         fp_arr = np.array(samples).T.astype(np.float32)
-#         fp_arr /= np.iinfo(samples[0].typecode).max
-#         return fp_arr
-#
-#     async def get_next_sample(self, side):
-#         if side == "left":
-#             return await self.left_encoded_queue.get()
-#
-#         elif side == "right":
-#             return await self.right_encoded_queue.get()
-#         else:
-#             raise Exception("Invalid side")
-#
-#     async def convert_audio(self, sample_size):
-#         input_audio = AudioSegment.from_file(self.audio_path, format="mp3")
-#         input_audio = input_audio.set_frame_rate(self.SAMPLE_RATE)
-#         input_data = AudioEncoder.pydub_to_np(input_audio)
-#         left_channel = input_data[:, 0]
-#         right_channel = input_data[:, 1]
-#         # self.left_encoded_queue = asyncio.Queue()
-#         # self.task_left = asyncio.create_task(
-#         #     self.encode_dfpwm(
-#         #         left_channel,
-#         #         sample_size,
-#         #         self.left_encoded_queue
-#         #     )
-#         # )
-#         self.right_encoded_queue = asyncio.Queue(maxsize=1)
-#         self.task_right = asyncio.create_task(
-#             self.encode_dfpwm(
-#                 right_channel,
-#                 sample_size,
-#                 self.right_encoded_queue
-#             )
-#         )
-#
-#     async def has_next_sample(self):
-#         right_task_done = self.task_right.done() if hasattr(self, "task_right") else True
-#         right_task_queue_empty = self.right_encoded_queue.empty() if hasattr(self, "right_encoded_queue") else True
-#         left_task_done = self.task_left.done() if hasattr(self, "task_left") else True
-#         left_task_queue_empty = self.left_encoded_queue.empty() if hasattr(self, "left_encoded_queue") else True
-#         return not (right_task_done and right_task_queue_empty and left_task_done and left_task_queue_empty)
-#
 
 class WebsocketServer:
     __alphabet__ = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -397,10 +304,14 @@ class WebsocketServer:
         raw_url = await websocket.recv()
         video_url = raw_url.strip()
         client_id = self.__gen_client_id__()
+        await websocket.send(client_id)
+        mode = await websocket.recv()
+        await websocket.close()
         # client_id = get_video_id_from_url(video_url)
         client = WebsocketClient(
                 client_id,
-                self.download_folder
+                self.download_folder,
+                mode
         )
         self.clients[client_id] = {
             "client": client,
@@ -408,8 +319,6 @@ class WebsocketServer:
             "audio": None
         }
         client.handle(video_url)
-        await websocket.send(client_id)
-        await websocket.close()
         print(f"Client '{client_id}' created")
 
 
@@ -417,10 +326,12 @@ class WebsocketClient:
     def __init__(
             self,
             client_id,
-            download_folder="../../download/"):
+            download_folder,
+            mode):
         print("Client connected...")
         self.client_id = client_id
         self.download_folder = download_folder
+        self.mode = mode
 
     def handle(self, video_url):
         print("Waiting for video url...")
@@ -431,15 +342,28 @@ class WebsocketClient:
         # if yes check if video.mp4 and audio.mp3 exist
         # if no delete the folder and create it
         # if yes do nothing
-        client_folder = os.path.join(self.download_folder, get_video_id_from_url(video_url))
+        client_folder = os.path.join(
+                self.download_folder,
+                get_video_id_from_url(video_url)
+        )
+        if client_folder is None:
+            print("Invalid video url")
+            return
         if not os.path.exists(client_folder):
             os.mkdir(client_folder)
         self.video_path, self.audio_path = download_video(
-                video_url, client_folder)
+                video_url,
+                client_folder
+        )
         print("Video downloaded")
         print("Starting video and audio encoding...")
-        self.video_encoder = VideoEncoder(self.video_path)
-        self.audio_encoder = AudioEncoder(self.audio_path)
+        self.video_encoder = VideoEncoder(
+                self.video_path
+        )
+        self.audio_encoder = AudioEncoder(
+                self.audio_path,
+                self.mode
+        )
 
     async def video_websocket(self, websocket):
         # first message is the size of the image
@@ -458,26 +382,18 @@ class WebsocketClient:
             print(f"Frame generated in {elapsed_time}ms")
             print(f"Ignored {ignored_frames} frames")
             await websocket.send(image_encoded)
-            response = await websocket.recv()
+            try:
+                response = await asyncio.wait_for(websocket.recv(), timeout=10)
+            except asyncio.TimeoutError:
+                print("Timeout")
+                break
+            except websockets.exceptions.ConnectionClosed:
+                print("Connection closed")
+                break
             width, height = [int(x.strip()) for x in response.split('x')]
         print("Finished sending frames")
         await websocket.send("stop")
         await websocket.close()
-
-    async def audio_websocket(self, websocket, side):
-        # itialize the client socket
-        # send to the client the size of the frame packets
-        # (1s of audio per packets)
-        sample_size = 16 * 1024
-        await websocket.send(str(sample_size))
-        for data in self.audio_encoder.frame_generator(sample_size, side):
-            await websocket.send(data)
-            await websocket.recv()
-        # for audio_data in audio_data_generator:
-        #     # send the audio data to the client
-        #     await websocket.send(audio_data.tobytes())
-        #     # wait client to be ready for the next packet
-        #     await websocket.recv()
 
 
 async def main(args):
